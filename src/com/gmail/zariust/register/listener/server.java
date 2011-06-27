911007624598f75692e9395331fed5891620ffd2
Zarius <zariust@gmail.com>
2011-06-27 22:44:42 +1000

    woah - forgot to commit server.java to the repo (for version 0.9.0)

---
+++ src/com/gmail/zariust/register/listener/server.java
@@ -0,0 +1,57 @@
+package com.gmail.zariust.register.listener;
+
+// Example plugin
+import com.gmail.zariust.register.payment.Method;
+import com.gmail.zariust.register.payment.Methods;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import com.sargant.bukkit.otherblocks.OtherBlocks;
+
+// Imports for Register
+
+// Bukkit Imports
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.ServerListener;
+import org.bukkit.plugin.Plugin;
+
+public class server extends ServerListener {
+    // Change "MyPlugin" to the name of your MAIN class file.
+    // Let's say my plugins MAIN class is: Register.java
+    // I would change "MyPlugin" to "Register"
+    private OtherBlocks plugin;
+    private Methods Methods = null;
+
+    public server(OtherBlocks plugin) {
+        this.plugin = plugin;
+        this.Methods = new Methods();
+    }
+
+    @Override
+    public void onPluginDisable(PluginDisableEvent event) {
+        // Check to see if the plugin thats being disabled is the one we are using
+        if (this.Methods != null && this.Methods.hasMethod()) {
+            Boolean check = this.Methods.checkDisabled(event.getPlugin());
+
+            if(check) {
+                OtherBlocks.Method = null;
+                System.out.println("[" + plugin.info.getName() + "] Payment method was disabled. No longer accepting payments.");
+            }
+        }
+
+    }
+
+    @Override
+    public void onPluginEnable(PluginEnableEvent event) {
+        // Check to see if we need a payment method
+        if (!this.Methods.hasMethod()) {
+            if(this.Methods.setMethod(event.getPlugin())) {
+                // You might want to make this a public variable inside your MAIN class public Method Method = null;
+                // then reference it through this.plugin.Method so that way you can use it in the rest of your plugin ;)
+                OtherBlocks.Method = this.Methods.getMethod();
+                System.out.println("[Otherblocks] Payment method found (" + this.plugin.Method.getName() + " version: " + this.plugin.Method.getVersion() + ")");
+//                System.out.println("[" + plugin.info.getName() + "] Payment method found (" + this.plugin.Method.getName() + " version: " + this.plugin.Method.getVersion() + ")");
+            }
+        }
+
+    }
+}
