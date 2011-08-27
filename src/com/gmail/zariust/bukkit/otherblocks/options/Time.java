e558e13478182a64d0e031132998f08beee4525e
Zarius <zariust@gmail.com>
2011-08-27 13:08:45 +1000

    fix time.parseFrom to read time option rather than weather

--- src/com/gmail/zariust/bukkit/otherblocks/options/Time.java
+++ src/com/gmail/zariust/bukkit/otherblocks/options/Time.java
@@ -60,7 +60,7 @@ public class Time extends Range<Long> {
 	}
 
 	public static Map<Time, Boolean> parseFrom(ConfigurationNode node, Map<Time, Boolean> def) {
-		List<String> times = OtherBlocksConfig.getMaybeList(node, "weather");
+		List<String> times = OtherBlocksConfig.getMaybeList(node, "time");
 		if(times.isEmpty()) return def;
 		HashMap<Time, Boolean> result = new HashMap<Time,Boolean>();
 		for(String name : times) {
