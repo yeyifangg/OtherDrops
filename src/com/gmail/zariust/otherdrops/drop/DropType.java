8f094bac562944af05865a73355f80266131b1f5
Celtic Minstrel <celtic.minstrel.ca@some.place>
2011-10-01 18:05:37 -0400

    CreatureData for Endermen, and SelfDrop no longer delegates to a temporary DropType instance. - Also, the DropType method for dropping a creature no longer manually sets the data; this was unnecessary code duplication.

--- src/com/gmail/zariust/otherdrops/drop/DropType.java
+++ src/com/gmail/zariust/otherdrops/drop/DropType.java
@@ -22,24 +22,17 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
-import org.bukkit.DyeColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.CreatureType;
-import org.bukkit.entity.Creeper;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Pig;
-import org.bukkit.entity.PigZombie;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Sheep;
-import org.bukkit.entity.Slime;
-import org.bukkit.entity.Wolf;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.config.ConfigurationNode;
 
 import com.gmail.zariust.otherdrops.OtherDropsConfig;
+import com.gmail.zariust.otherdrops.data.Data;
 import com.gmail.zariust.otherdrops.options.DoubleRange;
 import com.gmail.zariust.otherdrops.subject.Target;
 
@@ -132,40 +125,10 @@ public abstract class DropType {
 	}
 	
 	// Drop a creature!
-	protected static void drop(Location where, AnimalTamer owner, CreatureType type, int data) {
+	protected static void drop(Location where, Player owner, CreatureType type, Data data) {
 		World in = where.getWorld();
 		LivingEntity mob = in.spawnCreature(where, type);
-		switch(type) {
-		case CREEPER:
-			if(data == 1) ((Creeper)mob).setPowered(true);
-			break;
-		case PIG:
-			if(data == 1) ((Pig)mob).setSaddle(true);
-			break;
-		case SHEEP:
-			if(data >= 16) ((Sheep)mob).setSheared(true);
-			data -= 16;
-			((Sheep)mob).setColor(DyeColor.getByData((byte) data));
-			break;
-		case SLIME:
-			if(data > 0) ((Slime)mob).setSize(data);
-			break;
-		case WOLF:
-			switch(data) {
-			case 1:
-				((Wolf)mob).setAngry(true);
-				break;
-			case 2:
-				((Wolf)mob).setTamed(true);
-				((Wolf)mob).setOwner(owner);
-				break;
-			}
-			break;
-		case PIG_ZOMBIE:
-			if(data > 0) ((PigZombie)mob).setAnger(data);
-			break;
-		default:
-		}
+		data.setOn(mob, owner);
 	}
 
 	@SuppressWarnings("rawtypes")
