71457dc9be7a10afe4c286dc5d0fa826d5d91a89
Zarius <zariust@gmail.com>
2012-01-25 00:13:06 +1000

    fix npe issue caused by previous "cleanup" commit with loadconfig "drop message"

--- src/com/gmail/zariust/otherdrops/OtherDropsConfig.java
+++ src/com/gmail/zariust/otherdrops/OtherDropsConfig.java
@@ -408,9 +408,8 @@ public class OtherDropsConfig {
 			deny = true; // set to DENY (used later to set replacement block to null)
 			drop.setDropped(new ItemDrop(Material.AIR)); // set the drop to NOTHING
 		} else drop.setDropped(DropType.parseFrom(node));
-		//if (drop.getDropped() != null) 
-		OtherDrops.logInfo("Loading drop: " + drop.getAction() + " with " + drop.getTool() + " on " + drop.getTarget() + " -> " + drop.getDropped().toString(),HIGHEST);
-		//else OtherDrops.logInfo("Loading drop (failed? null or default drop): " + drop.getAction() + " with " + drop.getTool() + " on " + drop.getTarget() + " -> " + dropStr,HIGHEST);
+		if (drop.getDropped() != null) OtherDrops.logInfo("Loading drop: " + drop.getAction() + " with " + drop.getTool() + " on " + drop.getTarget() + " -> " + drop.getDropped().toString(),HIGHEST);
+		else OtherDrops.logInfo("Loading drop (null: failed or default drop): " + drop.getAction() + " with " + drop.getTool() + " on " + drop.getTarget() + " -> \'" + dropStr+"\"",HIGHEST);
 			
 		String quantityStr = node.getString("quantity");
 		if(quantityStr == null) drop.setQuantity(1);
