5fc119c46dacba8b8b5a298d5450cdc14a420a46
Zarius <zariust@gmail.com>
2013-02-19 02:54:39 +1000

    allow LoreNameCheck to apply to tool used to fire a projectile (or more accurately the item in the shooters hand)

--- src/com/gmail/zariust/otherdrops/parameters/conditions/LoreNameCheck.java
+++ src/com/gmail/zariust/otherdrops/parameters/conditions/LoreNameCheck.java
@@ -11,6 +11,7 @@ import com.gmail.zariust.otherdrops.ConfigurationNode;
 import com.gmail.zariust.otherdrops.Log;
 import com.gmail.zariust.otherdrops.event.OccurredEvent;
 import com.gmail.zariust.otherdrops.subject.PlayerSubject;
+import com.gmail.zariust.otherdrops.subject.ProjectileAgent;
 
 public class LoreNameCheck extends Condition {
 
@@ -25,18 +26,25 @@ public class LoreNameCheck extends Condition {
 	public boolean checkInstance(OccurredEvent occurrence) {
 		Log.logInfo("Checking for lorename condition..." + "== "+loreName, Verbosity.HIGHEST);
 		if (occurrence.getTool() instanceof PlayerSubject) {
-			PlayerSubject player = (PlayerSubject) occurrence.getTool();
-			ItemStack item = player.getPlayer().getItemInHand();
-			Log.logInfo("tool name = "+item.getType().name(), Verbosity.HIGHEST);
-			if (item.hasItemMeta()) {
-				String displayName = item.getItemMeta().getDisplayName();
-				Log.logInfo("Checking for lorename condition... '" + displayName + "' == '"+loreName+"'", Verbosity.HIGHEST);
-				if (displayName.equalsIgnoreCase(loreName)) return true;
+			return checkLoreName((PlayerSubject) occurrence.getTool());
+		} else if (occurrence.getTool() instanceof ProjectileAgent) {
+			ProjectileAgent pa = (ProjectileAgent) occurrence.getTool();
+			if (pa.getShooter() instanceof PlayerSubject) {
+				return checkLoreName((PlayerSubject)pa.getShooter());
 			}
 		}
-		
+		return false;		
+	}
+
+	private boolean checkLoreName(PlayerSubject player) {
+		ItemStack item = player.getPlayer().getItemInHand();
+		Log.logInfo("tool name = "+item.getType().name(), Verbosity.HIGHEST);
+		if (item.hasItemMeta()) {
+			String displayName = item.getItemMeta().getDisplayName();
+			Log.logInfo("Checking for lorename condition... '" + displayName + "' == '"+loreName+"'", Verbosity.HIGHEST);
+			if (displayName.equalsIgnoreCase(loreName)) return true;
+		}
 		return false;
-		
 	}
 
 	public static List<Condition> parse(ConfigurationNode node) {
