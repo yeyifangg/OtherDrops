46e75ab98343a3e754193e546f4ec319f9a86a65
Zarius <zariust@gmail.com>
2013-02-21 00:41:13 +1000

    ClearDropsAction started - incomplete & not live

---
+++ src/com/gmail/zariust/otherdrops/parameters/actions/ClearDropsAction.java
@@ -0,0 +1,58 @@
+package com.gmail.zariust.otherdrops.parameters.actions;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import com.gmail.zariust.common.Verbosity;
+import com.gmail.zariust.otherdrops.ConfigurationNode;
+import com.gmail.zariust.otherdrops.Log;
+import com.gmail.zariust.otherdrops.event.CustomDrop;
+import com.gmail.zariust.otherdrops.event.OccurredEvent;
+
+public class ClearDropsAction extends Action {
+
+	//private Collection<PotionEffect> effects = new ArrayList<PotionEffect>();
+	public enum ClearType {
+		XP, DROP, EQUIPMENT
+	}
+
+	private ClearType clearType;
+	
+	public ClearDropsAction(ClearType cType) {
+		this.clearType = cType;
+	}
+
+	@Override
+	public boolean act(CustomDrop drop, OccurredEvent occurence) {
+		
+		return true;
+	}
+
+	static List<Action> getClearXpAction() {
+		List<Action> actions = new ArrayList<Action>();
+		actions.add(new ClearDropsAction(ClearType.XP));
+		return actions;
+		
+	}
+
+	static List<Action> getClearDropsAction() {
+		List<Action> actions = new ArrayList<Action>();
+		actions.add(new ClearDropsAction(ClearType.DROP));
+		return actions;
+	}
+	static List<Action> getClearEquipmentAction() {
+		List<Action> actions = new ArrayList<Action>();
+		actions.add(new ClearDropsAction(ClearType.EQUIPMENT));
+		return actions;
+	}
+
+	@Override
+	public List<Action> parse(ConfigurationNode parseMe) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
