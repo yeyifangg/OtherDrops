f1b4853cacf3c404f54ebddd88085da9168fc5dd
Zarius <zariust@gmail.com>
2013-03-23 00:53:33 +1000

    Minor: refactor command handling, should operate the same but is much easier to read the code.

--- src/com/gmail/zariust/otherdrops/OtherDropsCommand.java
+++ src/com/gmail/zariust/otherdrops/OtherDropsCommand.java
@@ -44,25 +44,49 @@ import com.gmail.zariust.otherdrops.event.GroupDropEvent;
 import com.gmail.zariust.otherdrops.event.SimpleDrop;
 import com.gmail.zariust.otherdrops.options.Action;
 import com.gmail.zariust.otherdrops.parameters.actions.MessageAction;
+import com.gmail.zariust.otherdrops.subject.PlayerSubject;
 import com.gmail.zariust.otherdrops.subject.Target;
 import com.herocraftonline.heroes.characters.Hero;
 
 public class OtherDropsCommand implements CommandExecutor {
 	private enum OBCommand {
-		ID("id", "i"),
-		RELOAD("reload", "r"),
-		SHOW("show", "s"),
-		SETTINGS("settings", "st"),
-		DISABLE("disable,disabled,off", "o"),
-		ENABLE("enable,enabled,on", "e"),
-		HEROESTEST("heroestest", "ht"),
-		DROP("drop", "d");
+		ID(
+				"id", 
+				"i", 
+				"otherdrops.admin.id"),
+		RELOAD(
+				"reload", 
+				"r", 
+				"otherdrops.admin.reloadconfig"),
+		SHOW(
+				"show", 
+				"s", 
+				"otherdrops.admin.show"),
+		SETTINGS(
+				"settings", 
+				"st",
+				"otherdrops.admin.settings"),
+		DISABLE("disable,disabled,off", 
+				"o",
+				"otherdrops.admin.enabledisable"),
+		ENABLE("enable,enabled,on", 
+				"e",
+				"otherdrops.admin.enabledisable"),
+		HEROESTEST("heroestest", 
+				"ht",
+				""),
+		DROP(
+				"drop", 
+				"d",
+				"otherdrops.admin.drop");
 		private String cmdName;
 		private String cmdShort;
+		private String perm;
 
-		private OBCommand(String name, String abbr) {
+		private OBCommand(String name, String abbr, String perm) {
 			cmdName = name;
 			cmdShort = abbr;
+			this.perm = perm;
 		}
 		
 		public static OBCommand match(String label, String firstArg) {
@@ -89,6 +113,7 @@ public class OtherDropsCommand implements CommandExecutor {
 			return newArgs;
 		}
 	}
+	
 	private final OtherDrops otherdrops;
 	
 	public OtherDropsCommand(OtherDrops plugin) {
@@ -101,177 +126,209 @@ public class OtherDropsCommand implements CommandExecutor {
 		else return "UNKNOWN";
 	}
 	
-	// returns null if not durability is not valid (ie. Has no “maxdurability”)
-	private Double getDurabilityPercentage(ItemStack item) {
-		Double maxDura = Double.valueOf(item.getType().getMaxDurability());
-		Double dura = Double.valueOf(item.getDurability());
-		
-		if (maxDura < 1) return null;
-		return (double) (Math.round((float)(1-(dura/maxDura))*10000)/100);
-	}
-
-	public static Entity getTarget(final Player player) {
-		 
-        BlockIterator iterator = new BlockIterator(player.getWorld(), player
-                .getLocation().toVector(), player.getEyeLocation()
-                .getDirection(), 0, 100);
-        Entity target = null;
-        while (iterator.hasNext()) {
-            Block item = iterator.next();
-            for (Entity entity : player.getNearbyEntities(100, 100, 100)) {
-                int acc = 2;
-                for (int x = -acc; x < acc; x++)
-                    for (int z = -acc; z < acc; z++)
-                        for (int y = -acc; y < acc; y++)
-                            if (entity.getLocation().getBlock()
-                                    .getRelative(x, y, z).equals(item)) {
-                                return target = entity;
-                            }
-            }
-        }
-        return target;
-    }
-	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		OBCommand cmd = OBCommand.match(label, args.length >= 1 ? args[0] : "");
 		if(cmd == null) return false;
 		StringBuffer cmdName = new StringBuffer(label);
 		args = cmd.trim(args, cmdName);
+		
+		if (!checkCommandPermissions(sender, args, cmd)) return true;
+		
 		switch(cmd) {
 		case ID:
-			if(Dependencies.hasPermission(sender, "otherdrops.admin.id")) {
-				if (sender instanceof Player) {
-					Player player = (Player)sender;
-					ItemStack playerItem = player.getItemInHand();
-
-					if (args.length > 0 && args[0].toLowerCase().matches("(mob|creature)")) {
-						Entity mob = getTarget(player);
-						if (mob instanceof LivingEntity)
-							// TODO: parse via CreatureDrop (need to create CreatureDrop.parse(entity)
-							sender.sendMessage("OdId: mob details: "+mob.getType().toString()+"@"+CreatureData.parse(mob).toString());
-						else
-							sender.sendMessage("No living entity found.");
-					} else {
-						String itemMsg = "Item in hand: "+playerItem.getTypeId()+"@"+playerItem.getDurability()+" maxdura:"+playerItem.getType().getMaxDurability() + " dura%:"+getDurabilityPercentage(playerItem)+" detail: "+playerItem.toString();
-						if (playerItem.getItemMeta() != null && playerItem.getItemMeta().getDisplayName() != null) itemMsg += " lorename: \""+playerItem.getItemMeta().getDisplayName().replaceAll("§", "&")+"\"";
-						sender.sendMessage(itemMsg);
-						Block block = player.getTargetBlock(null, 100);
-						sender.sendMessage("Block looked at is "+block.toString()+" mat: "+block.getType().toString()+" lightlevel: "+block.getLightLevel() +" lightfromsky: "+block.getLightFromSky());
-					}
-				}
-			} else sender.sendMessage("You don't have permission for this command.");
+			cmdId(sender, args);
 			break;
 		case RELOAD:
-			if(Dependencies.hasPermission(sender, "otherdrops.admin.reloadconfig")) {
-				otherdrops.config.load();
-				sender.sendMessage("OtherDrops config reloaded.");
-				Log.logInfo("Config reloaded by " + getName(sender) + ".");
-			} else sender.sendMessage("You don't have permission to reload the config.");
+			cmdReload(sender);
 			break;
 		case SHOW:
-			if(args.length == 0) {
-				sender.sendMessage("Error, no block. Please use /" + cmdName + " <block>");
-				return true;
-			}
-			if(Dependencies.hasPermission(sender, "otherdrops.admin.show")) {
-				Target target = OtherDropsConfig.parseTarget(args[0]);
-				for(Action action : Action.values())
-					showBlockInfo(sender, action, target);
-			} else sender.sendMessage("You don't have permission to show the drops for a block.");
+			cmdShow(sender, args, cmdName);
 			break;
 		case SETTINGS:
-			if(Dependencies.hasPermission(sender, "otherdrops.admin.profiling")) {
-				sender.sendMessage("OtherDrops settings:");
-				sender.sendMessage((otherdrops.enabled ? ChatColor.GREEN+"OtherDrops enabled." : ChatColor.RED+"OtherDrops disabled."));
-				sender.sendMessage("Verbosity: "+ChatColor.GRAY+OtherDropsConfig.getVerbosity());				
-				sender.sendMessage("Disable XP if no default drop: "+ChatColor.GRAY+OtherDropsConfig.disableXpOnNonDefault);				
-				sender.sendMessage("Money Precision (for messages): "+ChatColor.GRAY+OtherDropsConfig.moneyPrecision);				
-				sender.sendMessage("Use unsafe enchantments: "+ChatColor.GRAY+OtherDropsConfig.enchantmentsUseUnsafe);				
-				sender.sendMessage("Ignore enchantment start/maxlevel: "+ChatColor.GRAY+OtherDropsConfig.enchantmentsIgnoreLevel);				
-			} else sender.sendMessage("You don't have permission for this command.");
-			
+			cmdSettings(sender);				
 			break;
 		case ENABLE:
-			if(Dependencies.hasPermission(sender, "otherdrops.admin.enabledisable")) {
-				if (!otherdrops.enabled) {
-					OtherDrops.enableOtherDrops();
-					sender.sendMessage(ChatColor.GREEN+"OtherDrops enabled.");
-				} else {
-					sender.sendMessage(ChatColor.GRAY+"OtherDrops is already enabled.");
-				}
-			} else sender.sendMessage("You don't have permission for this command.");
+			cmdEnable(sender);
 			break;
 		case DISABLE:
-			if(Dependencies.hasPermission(sender, "otherdrops.admin.enabledisable")) {
-				if (otherdrops.enabled) {
-					OtherDrops.disableOtherDrops();
-					sender.sendMessage(ChatColor.RED+"OtherDrops disabled.");
-				} else {
-					sender.sendMessage(ChatColor.GRAY+"OtherDrops is already disabled.");
-				}
-			} else sender.sendMessage("You don't have permission for this command.");
+			cmdDisable(sender);
 			break;
 		case DROP:
-			if(Dependencies.hasPermission(sender, "otherdrops.admin.drop")) {
-			  if (args.length > 0) {
-				Location loc = null;
-				Player player = null;
-				String playerName = "unknown";
-				if (sender instanceof Player) {
-					player = (Player)sender;
-					playerName = player.getDisplayName();
-					//loc = player.getLocation();
-					loc = player.getTargetBlock(null, 100).getLocation().add(0,1,0); // (???,  max distance)
-				}
-				
-				if (loc != null) {
-					String dropString = "";
-					for (String arg : args)
-						dropString += arg + " ";
-					
-					dropString = dropString.substring(0, dropString.length()-1);
-					DropType drop = DropType.parse(dropString, "");
-					if (drop == null) {
-						sender.sendMessage("ODDrop - failed to parse drop.");
-						return true;
-					}
-					
-					// TODO: allow a way to get a readable name from the DropType
-					// Note: we have to parse the lorename here as it's usually parsed in the actual event trigger (so we know playername)
-					if (drop.getLoreName() != null)
-						drop.setLoreName(MessageAction.parseVariables(drop.getLoreName(), playerName, "", "", "", "1"));
-					
-					DropFlags flags = DropType.flags(player, true, false, OtherDrops.rng);
-					DropResult dropResult = drop.drop(loc, (Target)null, (Location)null, 1, flags);
-					
-					String dropped = "[NOTHING]";
-					if (dropResult.droppedEntities != null) dropped = dropResult.getDroppedString();
-					sender.sendMessage("Dropped: "+dropResult.getQuantity()+"x"+dropped);
-				}
-			  } else {
-			  	sender.sendMessage("Usage: /odd <item/mob>@<data> - drops the given item or mob");
-			  }
-			} else sender.sendMessage("You don't have permission for this command.");
+			cmdDrop(sender, args);
 			break;
 		case HEROESTEST:
-			if (sender instanceof Player) {
-				Player playerSender = (Player)sender;
-				if (Dependencies.hasHeroes()) {
-					sender.sendMessage("Player is in class: "+playerSender.getDisplayName()+"->"+Dependencies.getHeroes().getCharacterManager().getHero(playerSender).getClass());
-					sender.sendMessage("Other players Heroes classes output to server.log");
-					for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-
-						Hero heroPlayer = Dependencies.getHeroes().getCharacterManager().getHero(player);
-						Log.logInfo("Player:"+player.getDisplayName()+"->"+heroPlayer.getHeroClass().toString() + "("+heroPlayer.getLevel()+")");
-					}
-				}
-			}
+			cmdHeroesTest(sender);
 		default:
 			break;
 
 		}
 		return true;
+	}
+
+	/**
+	 * @param sender
+	 * @param args
+	 * @param cmd
+	 */
+	private boolean checkCommandPermissions(CommandSender sender, String[] args, OBCommand cmd) {
+		boolean pass = false;
+		if (cmd.perm.isEmpty()) pass = true;
+		else if(Dependencies.hasPermission(sender, cmd.perm)) pass = true;
+			
+		if (!pass) sender.sendMessage("You don't have permission for this command.");
+		return pass;
+	}
+
+	/**
+	 * @param sender
+	 */
+	private void cmdHeroesTest(CommandSender sender) {
+		if (sender instanceof Player) {
+			Player playerSender = (Player)sender;
+			if (Dependencies.hasHeroes()) {
+				sender.sendMessage("Player is in class: "+playerSender.getDisplayName()+"->"+Dependencies.getHeroes().getCharacterManager().getHero(playerSender).getClass());
+				sender.sendMessage("Other players Heroes classes output to server.log");
+				for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+
+					Hero heroPlayer = Dependencies.getHeroes().getCharacterManager().getHero(player);
+					Log.logInfo("Player:"+player.getDisplayName()+"->"+heroPlayer.getHeroClass().toString() + "("+heroPlayer.getLevel()+")");
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param sender
+	 * @param args
+	 */
+	private void cmdDrop(CommandSender sender, String[] args) {
+		if (args.length > 0) {
+			Location loc = null;
+			Player player = null;
+			String playerName = "unknown";
+			if (sender instanceof Player) {
+				player = (Player)sender;
+				playerName = player.getDisplayName();
+				//loc = player.getLocation();
+				loc = player.getTargetBlock(null, 100).getLocation().add(0,1,0); // (???,  max distance)
+			}
+			
+			if (loc != null) {
+				String dropString = "";
+				for (String arg : args)
+					dropString += arg + " ";
+				
+				dropString = dropString.substring(0, dropString.length()-1);
+				DropType drop = DropType.parse(dropString, "");
+				if (drop == null) {
+					sender.sendMessage("ODDrop - failed to parse drop.");
+					return;
+				}
+				
+				// TODO: allow a way to get a readable name from the DropType
+				// Note: we have to parse the lorename here as it's usually parsed in the actual event trigger (so we know playername)
+				if (drop.getLoreName() != null)
+					drop.setLoreName(MessageAction.parseVariables(drop.getLoreName(), playerName, "", "", "", "1"));
+				
+				DropFlags flags = DropType.flags(player, new PlayerSubject(player), true, false, OtherDrops.rng);
+				DropResult dropResult = drop.drop(loc, (Target)null, (Location)null, 1, flags);
+				
+				String dropped = "[NOTHING]";
+				if (dropResult.droppedEntities != null) dropped = dropResult.getDroppedString();
+				sender.sendMessage("Dropped: "+dropResult.getQuantity()+"x"+dropped);
+			}
+		  } else {
+		  	sender.sendMessage("Usage: /odd <item/mob>@<data> - drops the given item or mob");
+		  }
+	}
+
+	/**
+	 * @param sender
+	 */
+	private void cmdDisable(CommandSender sender) {
+		if (otherdrops.enabled) {
+			OtherDrops.disableOtherDrops();
+			sender.sendMessage(ChatColor.RED+"OtherDrops disabled.");
+		} else {
+			sender.sendMessage(ChatColor.GRAY+"OtherDrops is already disabled.");
+		}
+	}
+
+	/**
+	 * @param sender
+	 */
+	private void cmdEnable(CommandSender sender) {
+		if (!otherdrops.enabled) {
+			OtherDrops.enableOtherDrops();
+			sender.sendMessage(ChatColor.GREEN+"OtherDrops enabled.");
+		} else {
+			sender.sendMessage(ChatColor.GRAY+"OtherDrops is already enabled.");
+		}
+	}
+
+	/**
+	 * @param sender
+	 */
+	private void cmdSettings(CommandSender sender) {
+		sender.sendMessage("OtherDrops settings:");
+		sender.sendMessage((otherdrops.enabled ? ChatColor.GREEN+"OtherDrops enabled." : ChatColor.RED+"OtherDrops disabled."));
+		sender.sendMessage("Verbosity: "+ChatColor.GRAY+OtherDropsConfig.getVerbosity());				
+		sender.sendMessage("Disable XP if no default drop: "+ChatColor.GRAY+OtherDropsConfig.disableXpOnNonDefault);				
+		sender.sendMessage("Money Precision (for messages): "+ChatColor.GRAY+OtherDropsConfig.moneyPrecision);				
+		sender.sendMessage("Use unsafe enchantments: "+ChatColor.GRAY+OtherDropsConfig.enchantmentsUseUnsafe);				
+		sender.sendMessage("Ignore enchantment start/maxlevel: "+ChatColor.GRAY+OtherDropsConfig.enchantmentsIgnoreLevel);
+	}
+
+	/**
+	 * @param sender
+	 * @param args
+	 * @param cmdName
+	 */
+	private void cmdShow(CommandSender sender, String[] args,
+			StringBuffer cmdName) {
+		if(args.length == 0) {
+			sender.sendMessage("Error, no block. Please use /" + cmdName + " <block>");
+			return;
+		}
+		Target target = OtherDropsConfig.parseTarget(args[0]);
+		for(Action action : Action.values())
+			showBlockInfo(sender, action, target);
+	}
+
+	/**
+	 * @param sender
+	 */
+	private void cmdReload(CommandSender sender) {
+		otherdrops.config.load();
+		sender.sendMessage("OtherDrops config reloaded.");
+		Log.logInfo("Config reloaded by " + getName(sender) + ".");
+	}
+
+	/**
+	 * @param sender
+	 * @param args
+	 */
+	private void cmdId(CommandSender sender, String[] args) {
+		if (sender instanceof Player) {
+			Player player = (Player)sender;
+			ItemStack playerItem = player.getItemInHand();
+
+			if (args.length > 0 && args[0].toLowerCase().matches("(mob|creature)")) {
+				Entity mob = getTarget(player);
+				if (mob instanceof LivingEntity)
+					// TODO: parse via CreatureDrop (need to create CreatureDrop.parse(entity)
+					sender.sendMessage("OdId: mob details: "+mob.getType().toString()+"@"+CreatureData.parse(mob).toString());
+				else
+					sender.sendMessage("No living entity found.");
+			} else {
+				String itemMsg = "Item in hand: "+playerItem.getTypeId()+"@"+playerItem.getDurability()+" maxdura:"+playerItem.getType().getMaxDurability() + " dura%:"+getDurabilityPercentage(playerItem)+" detail: "+playerItem.toString();
+				if (playerItem.getItemMeta() != null && playerItem.getItemMeta().getDisplayName() != null) itemMsg += " lorename: \""+playerItem.getItemMeta().getDisplayName().replaceAll("§", "&")+"\"";
+				sender.sendMessage(itemMsg);
+				Block block = player.getTargetBlock(null, 100);
+				sender.sendMessage("Block looked at is "+block.toString()+" mat: "+block.getType().toString()+" lightlevel: "+block.getLightLevel() +" lightfromsky: "+block.getLightFromSky());
+			}
+		}
 	}
 	
 	/* "/od show" command - shows conditions and actions for the specified block
@@ -365,5 +422,36 @@ public class OtherDropsCommand implements CommandExecutor {
 			if(subDrop instanceof GroupDropEvent) addDropInfo(message, (GroupDropEvent) subDrop);
 			else addDropInfo(message, (SimpleDrop) subDrop);
 		}
+	}
+
+	// returns null if not durability is not valid (ie. Has no “maxdurability”)
+	private Double getDurabilityPercentage(ItemStack item) {
+		Double maxDura = Double.valueOf(item.getType().getMaxDurability());
+		Double dura = Double.valueOf(item.getDurability());
+		
+		if (maxDura < 1) return null;
+		return (double) (Math.round((float)(1-(dura/maxDura))*10000)/100);
+	}
+
+	public static Entity getTarget(final Player player) {
+		 
+	    BlockIterator iterator = new BlockIterator(player.getWorld(), player
+	            .getLocation().toVector(), player.getEyeLocation()
+	            .getDirection(), 0, 100);
+	    Entity target = null;
+	    while (iterator.hasNext()) {
+	        Block item = iterator.next();
+	        for (Entity entity : player.getNearbyEntities(100, 100, 100)) {
+	            int acc = 2;
+	            for (int x = -acc; x < acc; x++)
+	                for (int z = -acc; z < acc; z++)
+	                    for (int y = -acc; y < acc; y++)
+	                        if (entity.getLocation().getBlock()
+	                                .getRelative(x, y, z).equals(item)) {
+	                            return target = entity;
+	                        }
+	        }
+	    }
+	    return target;
 	}
 }
